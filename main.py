f720d9f2af1e88b1d103b064028ec3627acafca0
sirGPS <gpszat@gmail.com>
2022-07-20 17:59:02 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+# KINO TINY SPINET
+
+# AUTHOR: GPS
+# DATE: 2022 07 20
+
+
+c61 = (922) # K
+c62 = (921) # I 
+c63 = (925) # N 
+c64 = (927) # O 
+
+
+print(chr(c61),chr(c62),chr(c63),chr(c64),sep = '')
